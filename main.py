2ea126872f1696f2459518ce962617ad4c1a4a3c
Skidd-McMarxx <14020610+Skidd-McMarxx@users.noreply.github.com>
2025-01-25 23:45:07 +0000

    added framerate limit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -11,6 +13,7 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
