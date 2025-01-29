f35d34d4b0b00ae8f81342dc37d988c33a7a10e5
Skidd-McMarxx <14020610+Skidd-McMarxx@users.noreply.github.com>
2025-01-29 18:50:46 +0000

    added groups and cleaned layout

--- main.py
+++ main.py
@@ -3,23 +3,37 @@ from constants import *
 from player import Player
 
 def main():
+    # Pygame 
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    
+    # Pygame Groups
+    updatable  = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable , drawable)
+    
+    dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # Game Loop
     while True:
+        # Exit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # Update
+        updatable.update(dt)
+        
+        # Draw
         pygame.Surface.fill(screen, (0,0,0))
-        player.draw(screen)
-        dt = clock.tick(60) / 1000
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
+        # FPS Limit
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
